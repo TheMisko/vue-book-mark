9e011edb11e043a45d7d4dcb7548a2a06da6a275
TheMisko <misko.199613@gmail.com>
2021-05-24 19:55:14 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar :marks="marks" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from "./components/SideBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      marks: [
+        {
+          group: "group 1",
+          marks: [
+            {
+              open: false,
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              open: true,
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              open: false,
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+          ],
+        },
+        {
+          group: "group 2",
+          marks: [
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+          ],
+        },
+        {
+          group: "group 3",
+          marks: [
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+            {
+              url: "https://urlname.com",
+              image: "httpUrlImage",
+              description: "lorem ipsum is a insert from latin",
+            },
+          ],
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    SideBar,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="sidebar-container">
+    <div class="marks-group" v-for="(mark, index) in marks" :key="mark.group">
+      {{ mark.group }} {{ index }}
+      <div v-if="(mark.open = true)">
+        <div v-for="groupMark in mark.marks" :key="groupMark.url">
+          {{ groupMark.url }}
+        </div>
+      </div>
+      <div v-else>yoo</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: ["marks"],
+};
+</script>
+
+<style scoped>
+.sidebar-container {
+  width: 17vw;
+  background-color: aquamarine;
+  height: 100vh;
+}
+.marks-group {
+  height: 10vh;
+  margin-bottom: 15px;
+  background-color: bisque;
+}
+</style>
