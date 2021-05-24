820ffc638577d6f491e9f3952b38385ce5c9d547
TheMisko <misko.199613@gmail.com>
2021-05-24 20:11:20 +0200

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <SideBar :marks="marks" />
+    <div>yoooo</div>
   </div>
 </template>
 
@@ -84,8 +85,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
+}
+#app {
+  display: flex;
+  background-color: aquamarine;
 }
 </style>
